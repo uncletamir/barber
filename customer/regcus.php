e11a0598c571c1d3cc163046e1880e00684ab11b
uncletamir <aryo.wibowo5@gmail.com>
2021-11-20 20:45:42 +0800

    change path regcus.php

--- customer/regcus.php
+++ customer/regcus.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once("connection.inc.php");
-require_once("function.inc.php");
+require_once("../connection.inc.php");
+require_once("../function.inc.php");
 
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
-require 'PHPMailer/src/Exception.php';
+require '../PHPMailer/src/PHPMailer.php';
+require '../PHPMailer/src/SMTP.php';
+require '../PHPMailer/src/Exception.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
