34aeda97844612eac7aa2fc8ea26a23c538b235d
uncletamir <aryo.wibowo5@gmail.com>
2021-11-20 20:03:02 +0800

    change sql connection

--- connection.inc.php
+++ connection.inc.php
@@ -1,6 +1,6 @@
 <?php
 //using mysqli
-$mysqli = new mysqli("localhost", "root", "", "db_barber");
+$mysqli = new mysqli("us-cdbr-east-04.cleardb.com", "bef139bdfd9ede", "1b2a3b8c", "heroku_6578c13f024066b");
 //$mysqli = new mysqli("localhost", "root", "", "db_barber-new");
 if ($mysqli->connect_errno) {
 	echo "Failed to connect to MySQL: " . $mysqli->connect_error;
